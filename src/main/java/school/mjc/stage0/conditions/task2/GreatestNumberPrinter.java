721b8183f24adcb8e9625650d7bb6f71af2d11eb
zanzibra <talbergaerika@gmail.com>
2022-10-25 21:57:40 +0300

    change to first >= second

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        if(first > second) {
+        if(first >= second) {
             System.out.println(first);
         } else {
             System.out.println(second);
